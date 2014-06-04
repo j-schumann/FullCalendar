86abe01a993e151468ce6daf3323dd9b2566d6e7
Ellie Admin <admin@ellievantgrey.com>
2014-06-04 10:43:08 +0200

    initial commit

---
+++ config/module.config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * OsmTools config
+ */
+return array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../public',
+            ),
+        ),
+    ),
+);
